8987bfd533d045d6907610fc7a92648429d1de00
NOVI <novvvvvi@gmail.com>
2019-06-25 16:13:28 -0500

    edit.component html and scss

--- angutron/src/app/app-routing.module.ts
+++ angutron/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewComponent } from './components/new/new.component';
+import { EditComponent } from './components/edit/edit.component';
 
 const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'new',
+        component: NewComponent
+    },
+    {
+        path: 'edit',
+        component: EditComponent
     }
 ];
 
