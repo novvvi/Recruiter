4cc0b30428ec9bb1e7ab013ff5dbc3fb04d297b6
ricardor1835 <rick.rivera1835@gmail.com>
2019-06-27 12:36:31 -0500

    made the add experience button push the current experience into the user experience array

--- angutron/src/app/components/edit/edit.component.ts
+++ angutron/src/app/components/edit/edit.component.ts
@@ -22,7 +22,8 @@ export class EditComponent implements OnInit {
       fullName: '',
       phoneNumber: '',
       email: '',
-      address: ''
+      address: '',
+      experience: []
     }
     this.experience = {
       type: '',
@@ -36,29 +37,45 @@ export class EditComponent implements OnInit {
   }
 
   onSubmit() {
+    this.newUser.experience.push(this.experience)
     this.errors = [];
     let observable = this._httpService.create(this.newUser);
     observable.subscribe(data => {
       if (data['message'] === "Success") {
         console.log("created author", data);
         console.log(data['data']['_id'])
-        this._httpService.createExp(data['data']['_id'], this.experience).subscribe(data => {
-          if (data['message'] === "Success") {
-            console.log("created author", data);
-          } else {
-            console.log(data)
-          }
-        })
-        // this._router.navigate(['/']);
+        // this._httpService.createExp(data['data']['_id'], this.experience).subscribe(data => {
+        //   if (data['message'] === "Success") {
+        //     console.log("created author", data);
+        //   } else {
+        //     console.log(data)
+        //   }
+        // })
+        this._router.navigate(['/']);
       } else {
         console.log(data)
-        // this.errors.push(data['error']['errors']['name']['message'])
+        console.log(data['error']['errors'])
+        this.errors.push(data['error']['errors']['fullName']['message'])
+        this.errors.push(data['error']['errors']['email']['message'])
+        this.errors.push(data['error']['errors']['phoneNumber']['message'])
+        this.errors.push(data['error']['errors']['experience.0.details']['message'])
+        this.errors.push(data['error']['errors']['experience.0.specialty']['message'])
       }
     })
   }
 
   addSkill() {
-    var add = "<div class='border rounded shadow experiance-info'><div style='height: 35px;width: 910px;margin-top: 10px; margin-left: 50px;'><label class='d-inline-block float-left d-xl-flex align-items-xl-center infolabel' style='color: rgb(175,178,187);margin-right: 55px;'>Type</label><select [(ngModel)]='experience.type' class='form-control d-inline-block float-left' style='height: 35px;width: 212px;'><optgroup label='Pick Type'><option name='experience.type' value='company' selected=''>Company</option><option name='experience.type' value='school'>School</option></optgroup></select><label class='d-inline-block float-left d-xl-flex align-items-xl-center infolabel' style='color: rgb(175,178,187); margin-left: 29px;width: 101px;'>Name</label><input [(ngModel)]='experience.name' name='experience.name' class='form-control d-inline-block float-left' type='text' style='height: 35px;width: 461px;'></div><div class='d-inline-block float-left' style='height: 44px;width: 910px;margin-top: 10px;margin-left: 50px;'><label style='color: rgb(175,178,187); margin-right: 60px;' class='d-inline-block float-left d-xl-flex align-items-xl-center infolabel'>Title</label><input [(ngModel)]='experience.title' name='experience.title' class='form-control d-inline-block float-left' type='text' style='height: 35px;width: 296px;'><label class='d-inline-block float-left d-xl-flex align-items-xl-center infolabel' style='color: rgb(175,178,187); margin-left: 22px;width: 60px;'>Start</label><input [(ngModel)]='experience.start' name='experience.start' class='form-control d-inline-block float-left' type='date' style='width: 168px;'><label class='d-inline-block float-left d-xl-flex align-items-xl-center infolabel' style='color: rgb(175,178,187); margin-left: 22px;width: 68px;'>End</label><input [(ngModel)]='experience.end' name='experience.end' class='form-control d-inline-block float-left' type='date' style='width: 168px;'></div><div class='d-inline-block float-left' style='height: 35px;width: 910px;margin-top: 10px;margin-left: 50px;'><label style='color: rgb(175,178,187); margin-right: 20px;' class='d-inline-block float-left d-xl-flex align-items-xl-center infolabel'>Specialty</label><input [(ngModel)]='experience.specialty' name='experience.specialty' class='form-control' type='text' style='height: 35px;width: 750px;'></div><label class='d-inline-block float-left d-xl-flex align-items-xl-center infolabel' style='display: block; color: rgb(175,178,187); margin-left: 50px;'>Detail</label><textarea [(ngModel)]='experience.details' name='experience.details' class='form-control' style='margin-right: 28px;width: 910px;margin-left: 50px;height: 155px;'></textarea></div>"
-    document.getElementById('skill').innerHTML += add;
+    this.newUser.experience.push(this.experience)
+    // var add = "<div class='border rounded shadow experiance-info'><div style='height: 35px;width: 910px;margin-top: 10px; margin-left: 50px;'><label class='d-inline-block float-left d-xl-flex align-items-xl-center infolabel' style='color: rgb(175,178,187);margin-right: 55px;'>Type</label><select [(ngModel)]='experience.type' class='form-control d-inline-block float-left' style='height: 35px;width: 212px;'><optgroup label='Pick Type'><option name='experience.type' value='company' selected=''>Company</option><option name='experience.type' value='school'>School</option></optgroup></select><label class='d-inline-block float-left d-xl-flex align-items-xl-center infolabel' style='color: rgb(175,178,187); margin-left: 29px;width: 101px;'>Name</label><input [(ngModel)]='experience.name' name='experience.name' class='form-control d-inline-block float-left' type='text' style='height: 35px;width: 461px;'></div><div class='d-inline-block float-left' style='height: 44px;width: 910px;margin-top: 10px;margin-left: 50px;'><label style='color: rgb(175,178,187); margin-right: 60px;' class='d-inline-block float-left d-xl-flex align-items-xl-center infolabel'>Title</label><input [(ngModel)]='experience.title' name='experience.title' class='form-control d-inline-block float-left' type='text' style='height: 35px;width: 296px;'><label class='d-inline-block float-left d-xl-flex align-items-xl-center infolabel' style='color: rgb(175,178,187); margin-left: 22px;width: 60px;'>Start</label><input [(ngModel)]='experience.start' name='experience.start' class='form-control d-inline-block float-left' type='date' style='width: 168px;'><label class='d-inline-block float-left d-xl-flex align-items-xl-center infolabel' style='color: rgb(175,178,187); margin-left: 22px;width: 68px;'>End</label><input [(ngModel)]='experience.end' name='experience.end' class='form-control d-inline-block float-left' type='date' style='width: 168px;'></div><div class='d-inline-block float-left' style='height: 35px;width: 910px;margin-top: 10px;margin-left: 50px;'><label style='color: rgb(175,178,187); margin-right: 20px;' class='d-inline-block float-left d-xl-flex align-items-xl-center infolabel'>Specialty</label><input [(ngModel)]='experience.specialty' name='experience.specialty' class='form-control' type='text' style='height: 35px;width: 750px;'></div><label class='d-inline-block float-left d-xl-flex align-items-xl-center infolabel' style='display: block; color: rgb(175,178,187); margin-left: 50px;'>Detail</label><textarea [(ngModel)]='experience.details' name='experience.details' class='form-control' style='margin-right: 28px;width: 910px;margin-left: 50px;height: 155px;'></textarea></div>"
+    // document.getElementById('skill').innerHTML += add;
+    this.experience = {
+      type: '',
+      name: '',
+      title: '',
+      specialty: '',
+      details: '',
+      start: '',
+      end: ''
+    }
   }
 }
